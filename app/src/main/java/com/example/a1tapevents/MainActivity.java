5df65153c71009772e4e92006844552d7943f953
neethu3101 <neethuliz2001@gmail.com>
2022-06-26 12:54:04 +0530

    Added User Registration and Authentication

--- app/src/main/java/com/example/a1tapevents/MainActivity.java
+++ app/src/main/java/com/example/a1tapevents/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.a1tapevents;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +28,14 @@ public class MainActivity extends AppCompatActivity {
     private Button login_btn;
     private TextView signup;
 
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         init();
         onClickFunctions();
@@ -35,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.password_login);
         login_btn = findViewById(R.id.button_login);
         signup = findViewById(R.id.signup_login);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void onClickFunctions() {
@@ -42,8 +56,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Login Function
-                Intent intent = new Intent(MainActivity.this,HomePage.class);
-                startActivity(intent);
+                String email_d = email.getText().toString();
+                String password_d = password.getText().toString();
+                if(TextUtils.isEmpty(email_d) && TextUtils.isEmpty(password_d)){
+                    Toast.makeText(MainActivity.this,"Please add your credentials",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.signInWithEmailAndPassword(email_d, password_d).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this, HomePage.class);
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
             }
         });
 
@@ -56,6 +88,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    /*
+    protected void onStart(){
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            Intent i = new Intent(MainActivity.this, HomePage.class);
+            startActivity(i);
+            this.finish();
+        }
+    }
+    */
     private void ApiCalls() {}
 }
