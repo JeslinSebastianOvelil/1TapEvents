9b04bd6c2b35b733274f4fa77a7e9bec7be10a7d
l1zmathews <lizmathews2023@cs.ajce.in>
2022-06-28 15:57:45 +0530

    Added catering service

--- app/src/main/java/com/example/a1tapevents/HomePage.java
+++ app/src/main/java/com/example/a1tapevents/HomePage.java
@@ -2,8 +2,10 @@ package com.example.a1tapevents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class HomePage extends AppCompatActivity {
@@ -16,7 +18,15 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+
+
      init();
+     logo3.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Intent i=new Intent(HomePage.this,Food.class);
+         }
+     });
     }
     private void init(){
          logo3 = findViewById(R.id.logo3_home);}
